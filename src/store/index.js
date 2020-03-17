da80fea6bfa11de0d1f9f2c78d47d71e7fd3ae86
Josu Soleguia <josu@ideable.net>
2020-03-17 15:08:43 +0100

    Historico de tiradas.

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 // import _default from "vuex";
 import ssot from "./modules/ssot";
+import history from "./modules/history";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    ssot: ssot
+    ssot: ssot,
+    history: history
   }
 });
---
+++ src/store/modules/history.js
@@ -0,0 +1,12 @@
+export default {
+  namespaced: true,
+  state: {
+    history: {}
+  },
+  getters: {},
+  mutations: {
+    // guardarTirada(state, partida, ronda, tirada) {
+      // state.jugadores.push(jugador);
+    // },
+  }
+}
